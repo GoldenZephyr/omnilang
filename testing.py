95677ce36f70c3f0049efd54898cdd19eb16ef0e
Aaron Ray <aaronray@mit.edu>
2026-01-14 17:50:01 -0500

    toy planner working

--- testing.py
+++ testing.py
@@ -55,7 +55,7 @@ class SymbolGenerator:
     restrictions: list[Restriction]
 
 
-@dataclass
+@dataclass(frozen=True)
 class Fact:
     head: str
     body: list[Symbol | SymbolGenerator]
@@ -63,10 +63,49 @@ class Fact:
     def to_tuple(self):
         return (self.head, *[b.identifier for b in self.body])
 
+    def __hash__(self):
+        return hash(self.to_tuple())
+
+    def __eq__(self, other):
+        if isinstance(other, Fact):
+            return hash(self) == hash(other)
+        return NotImplemented
+
 
 @dataclass
 class State:
-    facts: list[Fact]
+    facts: set[Fact]
+    # implicitly assume facts that aren't listed are False
+
+    def add_fact(self, fact):
+        self.facts.add(fact)
+
+    def remove_fact(self, fact):
+        self.facts.remove(fact)
+
+    def __contains__(self, e):
+        match e:
+            case Fact():
+                return e in self.facts
+            case State():
+                return all(element in self.facts for element in e.facts)
+            case PartialState():
+                return all(
+                    element in self.facts for element in e.positive_facts
+                ) and all(element not in self.facts for element in e.negative_facts)
+            case list():  # list of Facts (?)
+                return all(element in self.facts for element in e)
+            case _:
+                raise NotImplementedError(
+                    f"Cannot check containment for {type(e)} in {type(self)}"
+                )
+
+
+@dataclass
+class PartialState:
+    # When propagating a set of states through an action, we need to separately track unknown facts and negative facts
+    positive_facts: set[Fact]
+    negative_facts: set[Fact]
 
 
 @dataclass
@@ -150,16 +189,18 @@ def generate(quantified_set: QuantifiedSet):
 
 @dataclass
 class LiftedAction:
+    name: str
     params: list[Symbol]
     param_restrictions: list[list[Restriction]]
     # TODO: I *think* that lifted actions should be thought of having ungrounded facts which are actually slightly different from predicates? And in that case, I'm not sure there's actually a distinction between Lifted and GroundedActions?
-    precondition: list[Predicate]
+    precondition: PartialState
     positive_effect: list[Predicate]
     negative_effect: list[Predicate]
 
 
 @dataclass
 class GroundedAction:
+    name: str
     precondition: list[Fact]
     positive_effect: list[Fact]
     negative_effect: list[Fact]
@@ -191,7 +232,10 @@ def ground_predicate(predicate, binding):
 
 
 def bind_action(a, bindings):
-    grounded_precondition = [ground_predicate(p, bindings) for p in a.precondition]
+    # NOTE: currently we only support positive preconditions (...)
+    grounded_precondition = PartialState(
+        [ground_predicate(p, bindings) for p in a.precondition], {}
+    )
     grounded_positive_effects = [
         ground_predicate(p, bindings) for p in a.positive_effect
     ]
@@ -199,7 +243,10 @@ def bind_action(a, bindings):
         ground_predicate(p, bindings) for p in a.negative_effect
     ]
     return GroundedAction(
-        grounded_precondition, grounded_positive_effects, grounded_negative_effects
+        a.name,
+        grounded_precondition,
+        grounded_positive_effects,
+        grounded_negative_effects,
     )
 
 
@@ -245,6 +292,7 @@ for gs in generate(goal):
 
 
 move = LiftedAction(
+    "move",
     ["?p1", "?p2"],
     [[], []],
     [Fact("at", ["?p1"])],
@@ -256,3 +304,79 @@ actions = [move]
 symbols = ["p1", "p2"]
 for a in ground_actions(actions, symbols):
     print(a)
+
+
+def update_state(state: State, added_facts: list[Fact], deleted_facts: list[Fact]):
+    new_state = State({s for s in state.facts})
+    for af in added_facts:
+        new_state.add_fact(af)
+    for df in deleted_facts:
+        new_state.remove_fact(df)
+    return new_state
+
+
+def update_partial_state(
+    state: PartialState, added_facts: list[Fact], deleted_facts: list[Fact]
+):
+    pass
+
+
+def iterate_neighbors(actions, symbols, state):
+    for a in ground_actions(actions, symbols):
+        # 1. Filter for applicable actions (TODO: we can add some logic that lets us greatly reduce the size of ground_actions)
+        if a.precondition in state:
+            # 2. Apply action
+            new_state = update_state(state, a.positive_effect, a.negative_effect)
+            yield a, new_state
+
+
+s0 = State(
+    [
+        Fact("at", [Symbol("p1")]),
+        Fact("connected", [Symbol("p1"), Symbol("p2")]),
+        Fact("connected", [Symbol("p2"), Symbol("p3")]),
+    ]
+)
+print("Possible s1: ")
+for a, n in iterate_neighbors(actions, env_symbols, s0):
+    print("Action: ", a, " Next state: ", n)
+
+
+def forward_search(actions, symbols, s0, goal, max_depth=3):
+    def search(max_depth, state, current_depth):
+        if current_depth == max_depth:
+            return None
+        for action, neighbor in iterate_neighbors(actions, symbols, state):
+            if goal in neighbor:
+                return [action]
+
+            rest = search(max_depth, neighbor, current_depth + 1)
+            if rest is not None:
+                return [action] + rest
+
+    # Iterative deepening
+    for md in range(1, max_depth):
+        result = search(md, s0, 0)
+        if result is not None:
+            return result
+    # failed to plan at max depth
+
+
+move_real = LiftedAction(
+    "move",
+    ["?p1", "?p2"],
+    [[], []],
+    [Fact("at", ["?p1"]), Fact("connected", ["?p1", "?p2"])],
+    [Fact("at", ["?p2"])],
+    [Fact("at", ["?p1"])],
+)
+
+test_goal = PartialState({Fact("at", [Symbol("p3")])}, {})
+plan = forward_search([move_real], env_symbols, s0, test_goal)
+print("Plan: ", plan)
+
+
+# 1. forward search -- DONE
+# 2. action inversion
+#    * In general an action has ~2^N inverses where N is the number of action effects. (every effect can be inverted or left unchanged)
+# 3. "goal state" check is true when the search state matches the problem's intial state, *or a "pure abstraction" of the initial state*
